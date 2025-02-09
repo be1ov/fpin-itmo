2614499c6ec300fe0956185487c726500a764495
be1ov <b.e.1.o.v@yandex.ru>
2025-02-09 12:16:11 +0300

    fix: edu

--- front/src/actions/EducationAction.ts
+++ front/src/actions/EducationAction.ts
@@ -1,11 +1,9 @@
-import {AppDispatch} from "../redux/store.ts";
+import {store} from "../redux/store.ts";
 import axiosInstance from "../utils/axios.ts";
 import {educationSlice} from "../redux/slices/EducationSlice.ts";
 
 export const fetchEducationData = () => {
-    return (dispatch: AppDispatch) => {
-        axiosInstance.get("/v1/education/current").then(res => {
-            dispatch(educationSlice.actions.setEducationData(res.data));
-        })
-    }
+    axiosInstance.get("/v1/education/current").then(res => {
+        store.dispatch(educationSlice.actions.setEducationData(res.data));
+    })
 }
