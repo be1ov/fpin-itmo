9dac43c3295d14d03672248794c516f46a6798fb
be1ov <b.e.1.o.v@yandex.ru>
2025-02-09 13:41:30 +0300

    fix: edu loading

--- front/src/actions/EducationAction.ts
+++ front/src/actions/EducationAction.ts
@@ -1,9 +1,13 @@
 import {store} from "../redux/store.ts";
 import axiosInstance from "../utils/axios.ts";
 import {educationSlice} from "../redux/slices/EducationSlice.ts";
+import {authSlice} from "../redux/slices/AuthSlice.ts";
 
 export const fetchEducationData = () => {
+    store.dispatch(authSlice.actions.setUserLoading())
     axiosInstance.get("/v1/education/current").then(res => {
         store.dispatch(educationSlice.actions.setEducationData(res.data));
+    }).finally(() => {
+        store.dispatch(authSlice.actions.setUserIdle())
     })
 }
