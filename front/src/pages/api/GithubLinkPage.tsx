ce9ac44b55924f149de0280b7e1c0e490db6fcba
be1ov <b.e.1.o.v@yandex.ru>
2025-03-04 23:37:12 +0300

    fix: gh link

--- front/src/pages/api/GithubLinkPage.tsx
+++ front/src/pages/api/GithubLinkPage.tsx
@@ -1,13 +1,11 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import {useCallback, useEffect, useRef} from "react";
+import { useLocation} from "react-router-dom";
+import {useEffect, useRef} from "react";
 import axiosInstance from "../../utils/axios.ts";
 
 export function GithubLinkPage() {
     const { search } = useLocation();
-    const navigate = useNavigate();
     const hasRun = useRef(false);
 
-    const stableNavigate = useCallback(navigate, []);
     useEffect(() => {
         if (hasRun.current) return;
         hasRun.current = true;
@@ -16,14 +14,21 @@ export function GithubLinkPage() {
         const code = query.get('code');
 
         if (!code) {
-            stableNavigate("/?status=gh_error");
+            window.location.href = "/?status=gh_error";
             return;
         }
 
         axiosInstance.post("/v1/github/link/", { code })
-            .then(() => stableNavigate("/?status=gh_success"))
-            .catch(() => stableNavigate("/?status=gh_error"));
-    }, [search, stableNavigate]);
+            .then((res) => {
+                console.log("API Response:", res);
+                window.location.href = "/?status=gh_success";
+            })
+            .catch((err) => {
+                console.log("API Error:", err);
+                window.location.href = "/?status=gh_error";
+            });
+
+    }, [search]);
 
 
     return <>Processing...</>;
