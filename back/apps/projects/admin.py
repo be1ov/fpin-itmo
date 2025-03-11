6d7e5ccf6453292816763cfd360a3fe82ed1d911
be1ov <b.e.1.o.v@yandex.ru>
2025-03-11 13:47:16 +0300

    feat: projects

--- back/apps/projects/admin.py
+++ back/apps/projects/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.projects.models import Project, ProjectParticipant, ProjectStatus, ProjectTask, ProjectTaskStatus, \
+    SemesterProject
+
+
+@admin.register(Project)
+class ProjectParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProjectParticipant)
+class ProjectParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProjectStatus)
+class ProjectTaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProjectTask)
+class ProjectTaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProjectTaskStatus)
+class SemesterProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SemesterProject)
+class SemesterProjectAdmin(admin.ModelAdmin):
+    pass
