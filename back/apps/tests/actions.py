e4c45b8fb0403f4127985c3229749141c5ddccdb
be1ov <b.e.1.o.v@yandex.ru>
2025-03-18 13:05:37 +0300

    feat: filter tests by assignment date

--- back/apps/tests/actions.py
+++ back/apps/tests/actions.py
@@ -1,8 +1,9 @@
 import typing as tp
+from datetime import datetime
 
 from apps.education.models import Flow
 from apps.tests.models import TestAssignment
 
 def get_assigned_tests(flows: tp.List[Flow]) -> tp.List[TestAssignment]:
-    return TestAssignment.objects.filter(flow__in=flows).all()
+    return TestAssignment.objects.filter(flow__in=flows, assignment_date__lte=datetime.now()).all()
 
