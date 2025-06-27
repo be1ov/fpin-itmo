366009941b46e208ef91453d244f995216a45956
be1ov <b.e.1.o.v@yandex.ru>
2025-06-27 17:19:51 +0300

    feat: google sheet integration

--- google_sheets/config.py
+++ google_sheets/config.py
@@ -1,9 +1,9 @@
-from pydantic import RedisDsn
+from pydantic import RedisDsn, Field
 from pydantic_settings import BaseSettings
 
 class CelerySettings(BaseSettings):
-    broker_url: RedisDsn
-    result_backend: RedisDsn
+    broker_url: RedisDsn = Field(alias="CELERY_BROKER_URL")
+    result_backend: RedisDsn = Field(alias="CELERY_RESULT_BACKEND")
     timezone: str = "Europe/Moscow"
 
     model_config = {
